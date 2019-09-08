741281dfd4bdfe6b81703cdd0d0f55ca0065d068
Vikas Meena <vikasbarlan1@gmail.com>
2019-09-09 01:02:06 +0530

    Create Demo.java

---
+++ Demo.java
@@ -0,0 +1,19 @@
+/filename and class name should not be same
+/if we declare it without public
+
+
+class Check{
+ public static void main(Stirngs[] args){
+  System.out.println(" Hello Brazil");
+ }
+}
+
+/to compile Demo.java
+/Step1 :
+/$javac Demo.java
+/it will create a Check.class (byte code) without any error message since the class is not public.
+
+/to run 
+/Step2 :
+ /$ java Check
+ 
