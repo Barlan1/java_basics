6e3bad562858f2c56eef3b2f26b08aad25b4cf8d
Vikas Meena <vikasbarlan1@gmail.com>
2019-09-09 01:13:22 +0530

    Create two_class_demo.java

---
+++ two_class_demo.java
@@ -0,0 +1,20 @@
+class Check
+{
+ public static void main(String[] args) {
+  System.out.println("Hello Brazil");
+ }
+}
+
+class CheckTest
+{ 
+ public static void main(String[] args){
+  System.out.println("Hello test class");
+ }
+}  
+
+// compiling 
+// javac two_class_demo.java
+// java$ java Check
+Hello Brazil
+ //java$ java CheckTest
+Hello test class
