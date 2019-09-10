77d423621a9bd670672645e0472c9b5c4fc41ac1
Vikas Meena <vikasbarlan1@gmail.com>
2019-09-10 21:35:58 +0530

    Create boolDemo.java

---
+++ boolDemo.java
@@ -0,0 +1,13 @@
+public class boolDemo
+{
+ public static void main(String[] args)
+ {
+
+  boolean b = true;
+  if(b==false)
+   System.out.println("hi Jack");
+  else
+   System.out.println("hello ji");
+   
+ }
+}
