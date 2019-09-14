c93471a2ffb61316d426cf2c28796b1b200fb8de
Vikas Meena <vikasbarlan1@gmail.com>
2019-09-15 01:02:48 +0530

    Add files via upload

---
+++ add.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int a,b,c;
+ a=2;
+ b=3;
+ c= a+b;
+cout<<"the sum of a and b is : "<<c;
+return 0;
+}
---
+++ mul.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b;
+cout<<"insert a \n";
+cin>>a;
+cout<<"insert b \n";
+cin>>b;
+cout<< "the output of a*b is"<< a*b;
+return 0;
+}
