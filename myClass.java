0a7d396583c77f97c7c92096d8bf8ed6049f3e9a
Vikas Meena <vikasbarlan1@gmail.com>
2019-09-26 14:43:13 +0530

    Create myClass.java

---
+++ myClass.java
@@ -0,0 +1,9 @@
+public class myClass {
+  int x = 5;
+  public static void main(String[] args) {
+   myClass obj1 = new myClass();
+   myClass obj2 = new myClass();
+   System.out.println(obj1.x*8*82);
+   System.out.println(obj2.x/2);
+  }
+}
