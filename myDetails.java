6d83c073bd873d0e0795eddc451548e05681e6d8
Vikas Meena <vikasbarlan1@gmail.com>
2019-09-26 13:55:47 +0530

    Create myDetails.java

---
+++ myDetails.java
@@ -0,0 +1,13 @@
+public class myDetails {
+public void myAge()
+{ 
+ int age = 10;
+ age = age + 16;
+ Stystem.out.println(" my age is : " + age);
+ }
+ public static void main(System args[])
+ {
+  myDetails obj = new myDetails();
+  obj.StudentAge();
+  }
+  
