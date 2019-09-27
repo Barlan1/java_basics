1d07aadf2963cc4705660f093dc2fe588bad3c13
Vikas Meena <vikasbarlan1@gmail.com>
2019-09-27 22:24:44 +0530

    Create subSimple.java

---
+++ subSimple.java
@@ -0,0 +1,10 @@
+package com.Barlan1.core;
+class subSimple {
+ public static void main(String args[])
+ System.out.println("Welcome to The subpackage ");
+ }
+}
+
+
+// compile $javac -d . subSimple.java
+//run  $java com.Barlan1.core.subSimple
