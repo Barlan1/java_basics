82c8e9dc8a8a034c609c02b828365680e37512ff
Vikas Meena <vikasbarlan1@gmail.com>
2019-09-28 19:29:43 +0530

    Create ifexample.java

---
+++ ifexample.java
@@ -0,0 +1,11 @@
+public class ifexample {
+ public static void main(String args[]) {
+  int marks = 33;
+  if (marks >= 35) {
+   System.out.println( " You are passed ");
+   }
+   else
+   System.out.println( " Fail ");
+  }
+ }  
+  
