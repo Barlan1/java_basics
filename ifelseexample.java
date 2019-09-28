71708dc8f9f0d6912ba445f6d480e0a19a965dab
Vikas Meena <vikasbarlan1@gmail.com>
2019-09-28 19:52:24 +0530

    Create ifelseexample.java

---
+++ ifelseexample.java
@@ -0,0 +1,13 @@
+public class ifelseexample {
+ public static void main (String[] args) {
+ int number = 45;
+ if(number % 2 == 0) {
+ System.out.println(" this is even number");
+ }
+ 
+ else
+ {
+ System.out.println("this is odd number");
+ }
+ }
+}
