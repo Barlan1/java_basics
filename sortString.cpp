229fae52b0c705ac84cbe7a1900463ef1bb795dd
Vikas Meena <vikasbarlan1@gmail.com>
2019-10-12 19:34:19 +0530

    Create sortString.cpp

---
+++ sortString.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+void sortString(&str)
+{ 
+ sort(str.begin(), string.end());
+ cout<<str;
+ }
+ int main()
+ {
+  string str= "omgwhoheis";
+  sortString(str);
+  return 0;
+  }
