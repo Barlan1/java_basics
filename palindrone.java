8fb47027a09784e92eff1e6b72be906a34718a25
Vikas Meena <vikasbarlan1@gmail.com>
2019-10-21 02:52:05 +0530

    Create palindrone.java

---
+++ palindrone.java
@@ -0,0 +1,18 @@
+public class palindrome{
+static boolean isPalindrome(String s)
+{
+int i=0, j=s.length()-1;
+while(i<j) {
+if(s.charAt(i) != s.charAt()
+return false;
+}
+return true;
+}
+public static void main (String[] args) {
+String s="malyalam";
+if(isPalindrome(s))
+System.out.print("yes");
+else
+System.out.print("no");
+}
+}
